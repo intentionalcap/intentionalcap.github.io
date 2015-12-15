5f8453b178b0d6595d73c7aac5128e6d31664bff
samullman <samullman@gmail.com>
2015-12-15 12:22:16 -0800

    styles changes:

--- js/main.js
+++ js/main.js
@@ -56,18 +56,8 @@ function main() {
   	  $("#team").owlCarousel({
 
   	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
+          slideSpeed : 300,
   	      paginationSpeed : 400,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
   	  });
 
   	  $("#clients").owlCarousel({
