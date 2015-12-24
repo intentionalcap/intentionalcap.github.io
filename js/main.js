d6ca02d9bc545a4f42f8a57b44d20a22bae1edac
samullman <samullman@gmail.com>
2015-12-24 13:24:16 -0800

    new mailer

--- js/main.js
+++ js/main.js
@@ -88,16 +88,43 @@ function main() {
     $(window).load(function() {
 
       $(document).ready(function() {
-        $(".contact-submit").click(function() {
+        function sendEmail () {
           $.ajax({
-              url: "//formspree.io/samullman@gmail.com",
-              method: "POST",
-              data: {message: "hello!"},
-              dataType: "json",
-              success: function (a, b, c) {
-                debugger
-              }
+            url: "//formspree.io/samullman@gmail.com",
+            method: "POST",
+            data: {
+              name: $('.contact-name').val(),
+              organization: $('.contact-organization').val(),
+              position: $('.contact-position').val(),
+              email: $('.contact-email').val(),
+              subject: $('.contact-subject').val(),
+              message: $('.contact-message').val(),
+            },
+            dataType: "json",
+            success: function (a, b, c) {
+              $('.contact-name').val('');
+              $('.contact-organization').val('');
+              $('.contact-position').val('');
+              $('.contact-email').val('');
+              $('.contact-subject').val('');
+              $('.contact-message').val('');
+            }
           });
+        }
+
+        $(".contact-form").submit(function(e) {
+          e.preventDefault();
+
+          if ( $('.contact-name').val() == '' ||
+               $('.contact-organization').val() == '' ||
+               $('.contact-position').val() == '' ||
+               $('.contact-email').val() == '' ||
+               $('.contact-subject').val() == '' ||
+               $('.contact-message').val() == '') {
+                 alert('Please fill out all required fields')
+               } else {
+                 sendEmail();
+               }
         });
       });
         // var $container = $('#themes-boxes');
